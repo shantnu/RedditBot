b716ad8c789d4ed59f2f251753643eeebc4875e2
shantnu <a@a.com>
2014-11-17 22:05:33 +0000

    Adding Comments. Pep8

--- Part 2/config_skel.py
+++ Part 2/config_skel.py
@@ -3,6 +3,5 @@
 
 # Fill in the values and rename this file to config_bot.py
 
-
-REDDIT_USERNAME = '' #YOUR USERNAME as string
-REDDIT_PASS = '' # YOUR PASSWORD as string
+REDDIT_USERNAME = ''  # YOUR USERNAME as string
+REDDIT_PASS = ''  # YOUR PASSWORD as string
--- Part 2/reply_post.py
+++ Part 2/reply_post.py
@@ -4,34 +4,49 @@ import re
 import os
 from config_bot import *
 
-if not  os.path.isfile("config_bot.py"):
+# Check that the file that contains our username exists
+if not os.path.isfile("config_bot.py"):
     print "You must create a config file with your username and password."
     print "Please see config_skel.py"
     exit(1)
-    
+
+# Create the Reddit instance
 user_agent = ("PyFor Eng bot 0.1")
 r = praw.Reddit(user_agent=user_agent)
+
+# and login
 r.login(REDDIT_USERNAME, REDDIT_PASS)
 
-
-if not  os.path.isfile("posts_replied_to.txt"):
+# Have we run this code before? If not, create an empty list
+if not os.path.isfile("posts_replied_to.txt"):
     posts_replied_to = []
+
+# If we have run the code before, load the list of posts we have replied to
 else:
+    # Read the file into a list and remove any empty values
     with open("posts_replied_to.txt", "r") as f:
-       posts_replied_to = f.read()
-       posts_replied_to = posts_replied_to.split("\n")
-       posts_replied_to = filter(None, posts_replied_to)
+        posts_replied_to = f.read()
+        posts_replied_to = posts_replied_to.split("\n")
+        posts_replied_to = filter(None, posts_replied_to)
 
-
+# Get the top 5 values from our subreddit
 subreddit = r.get_subreddit('pythonforengineers')
 for submission in subreddit.get_hot(limit=5):
-    #print submission.title
-    if submission.id not in posts_replied_to:
-      if re.search("i love python", submission.title, re.IGNORECASE):
-          submission.add_comment("Botty bot says: Me too!!")
-          print "Bot replying to : ", submission.title
-          posts_replied_to.append(submission.id)
+    # print submission.title
 
+    # If we haven't replied to this post before
+    if submission.id not in posts_replied_to:
+
+        # Do a case insensitive search
+        if re.search("i love python", submission.title, re.IGNORECASE):
+            # Reply to the post
+            submission.add_comment("Botty bot says: Me too!!")
+            print "Bot replying to : ", submission.title
+
+            # Store the current id into our list
+            posts_replied_to.append(submission.id)
+
+# Write our updated list back to the file
 with open("posts_replied_to.txt", "w") as f:
     for post_id in posts_replied_to:
         f.write(post_id + "\n")
