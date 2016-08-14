758bf971464aec5bee2f5b2ccf85b1eb269be100
Rich Gautier <rgautier@gmail.com>
2016-08-14 17:55:16 -0400

    Update for ignore files, code changes to reply_post for OAUTH2

--- Part2/reply_post.py
+++ Part2/reply_post.py
@@ -3,6 +3,7 @@ import praw
 import pdb
 import re
 import os
+import sys
 from config_bot import *
 
 # Check that the file that contains our username exists
@@ -12,12 +13,12 @@ if not os.path.isfile("config_bot.py"):
     exit(1)
 
 # Create the Reddit instance
-user_agent = ("PyFor Eng bot 0.1")
+user_agent = ("SemiAmusingBot 0.2")
 r = praw.Reddit(user_agent=user_agent)
 
-# and login
-r.login(REDDIT_USERNAME, REDDIT_PASS)
-
+# and login (DON'T USE THIS AGAIN)
+#r.login(REDDIT_USERNAME, REDDIT_PASS)
+r.refresh_access_information()
 # Have we run this code before? If not, create an empty list
 if not os.path.isfile("posts_replied_to.txt"):
     posts_replied_to = []
@@ -32,7 +33,9 @@ else:
 
 # Get the top 5 values from our subreddit
 subreddit = r.get_subreddit('pythonforengineers')
-for submission in subreddit.get_hot(limit=5):
+
+f = open("posts_replied_to.txt","a")
+for submission in subreddit.get_hot(limit=50):
     # print submission.title
 
     # If we haven't replied to this post before
@@ -41,13 +44,20 @@ for submission in subreddit.get_hot(limit=5):
         # Do a case insensitive search
         if re.search("i love python", submission.title, re.IGNORECASE):
             # Reply to the post
-            submission.add_comment("Nigerian scammer bot says: It's all about the Bass (and Python)")
-            print "Bot replying to : ", submission.title
+            try:
+                submission.add_comment("SemiAmusingBot says: They're very gentle snakes.")
+            except:
+		print "Error: ", sys.exc_info()[0]
+                print "Probably rate limited cuz you're a bot."
+                f.close()
+                quit()
+            else:
+                print "Bot replying to : ", submission.title
+                # write these out as we go so if we get rate limited, we have the data 
+                f.write(submission.id + "\n")
+      
+                # Store the current id into our list
+                posts_replied_to.append(submission.id)
 
-            # Store the current id into our list
-            posts_replied_to.append(submission.id)
-
-# Write our updated list back to the file
-with open("posts_replied_to.txt", "w") as f:
-    for post_id in posts_replied_to:
-        f.write(post_id + "\n")
+# Close out the submissions replied to
+f.close()
