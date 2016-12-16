123e4112f77e421d98b057a45f249996b39f7080
Shantnu Tiwari <shantnu@users.noreply.github.com>
2016-12-16 12:27:05 +0000

    Updating to Python3 and Praw 4

--- Part1/bot_read.py
+++ Part1/bot_read.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python
 import praw
 
-user_agent = ("PyEng Bot 0.1")
+reddit = praw.Reddit('bot1')
 
-r = praw.Reddit(user_agent = user_agent)
+subreddit = reddit.subreddit("learnpython")
 
-subreddit = r.get_subreddit("learnpython")
-
-for submission in subreddit.get_hot(limit = 5):
-    print "Title: ", submission.title
-    print "Text: ", submission.selftext
-    print "Score: ", submission.score
-    print "---------------------------------\n"
+for submission in subreddit.hot(limit=5):
+    print("Title: ", submission.title)
+    print("Text: ", submission.selftext)
+    print("Score: ", submission.score)
+    print("---------------------------------\n")
--- Part2/reply_post.py
+++ Part2/reply_post.py
@@ -3,20 +3,13 @@ import praw
 import pdb
 import re
 import os
-from config_bot import *
 
-# Check that the file that contains our username exists
-if not os.path.isfile("config_bot.py"):
-    print "You must create a config file with your username and password."
-    print "Please see config_skel.py"
-    exit(1)
 
 # Create the Reddit instance
-user_agent = ("PyFor Eng bot 0.1")
-r = praw.Reddit(user_agent=user_agent)
+reddit = praw.Reddit('bot1')
 
 # and login
-r.login(REDDIT_USERNAME, REDDIT_PASS)
+#reddit.login(REDDIT_USERNAME, REDDIT_PASS)
 
 # Have we run this code before? If not, create an empty list
 if not os.path.isfile("posts_replied_to.txt"):
@@ -28,12 +21,12 @@ else:
     with open("posts_replied_to.txt", "r") as f:
         posts_replied_to = f.read()
         posts_replied_to = posts_replied_to.split("\n")
-        posts_replied_to = filter(None, posts_replied_to)
+        posts_replied_to = list(filter(None, posts_replied_to))
 
 # Get the top 5 values from our subreddit
-subreddit = r.get_subreddit('pythonforengineers')
-for submission in subreddit.get_hot(limit=5):
-    # print submission.title
+subreddit = reddit.subreddit('pythonforengineers')
+for submission in subreddit.hot(limit=10):
+    #print(submission.title)
 
     # If we haven't replied to this post before
     if submission.id not in posts_replied_to:
@@ -41,8 +34,8 @@ for submission in subreddit.get_hot(limit=5):
         # Do a case insensitive search
         if re.search("i love python", submission.title, re.IGNORECASE):
             # Reply to the post
-            submission.add_comment("Nigerian scammer bot says: It's all about the Bass (and Python)")
-            print "Bot replying to : ", submission.title
+            submission.reply("Nigerian scammer bot says: It's all about the Bass (and Python)")
+            print("Bot replying to : ", submission.title)
 
             # Store the current id into our list
             posts_replied_to.append(submission.id)
