ac58f4fccd022c578c61e8b89998e6deb4aa9184
Shantnu Tiwari <shantnu@users.noreply.github.com>
2016-12-16 12:39:50 +0000

    removing unecessary file

--- Part2/config_skel.py
+++
@@ -1,7 +0,0 @@
-# This file contains the username and password to your bot.
-# DO NOT check it in!
-
-# Fill in the values and rename this file to config_bot.py
-
-REDDIT_USERNAME = ''  # YOUR USERNAME as string
-REDDIT_PASS = ''  # YOUR PASSWORD as string
