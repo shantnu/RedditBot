b133e00c010fb90f2c2f084937ec4a518b301c1c
aaaelite21 <alexander_ali@my.uri.edu>
2017-03-05 18:25:26 -0500

    commented out next steps, have final vision. Fixed html/css side of things

--- Part1/bot_read.py
+++ Part1/bot_read.py
@@ -7,25 +7,49 @@ from praw.models import MoreComments
 
 #create reddit instance
 reddit = praw.Reddit('bot1')
-
-subreddit = reddit.subreddit("modernmagic")
+#get the desired sub reddits
+subs=["gaming"]
+#all the html will be stored here
 elm=[]
+vsd=[]
+#read in visite submissions as to avoid them
+#
+#
+#read in old html as to save it
+#
+#
 
-for submission in subreddit.hot(limit=5):
-    #get comments and print relavent data
-    submission.comments.replace_more(limit=0)
-    for comment in submission.comments.list():
-        if comment.body.find("the") != -1:
-                title = submission.title
-                score = submission.score
-                url = submission.url
-                cmt = comment.body
-                if len(cmt) > 13:
-                    cmt = cmt[:len(cmt)-10] +"..."
-                elm.append(str("<div class=\"rlink\"><img src=\"rlogo.jpg\" alt=\"link or default reddit icon\" /><div class=\"rinfo\"><span class=\"rtitle\">"+title+"</span><br /><span class=\"rtext\">"+cmt+"</span></div><div class=\"points\">"+str(score)+"</div></div>"))
-                break
-    print("---------------------------------\n")
+
+
+for s in subs:
+    subreddit = reddit.subreddit(s)
+    for submission in subreddit.hot(limit=5):
+        #get comments and print relavent data
+        #check if the submission has allready been checked
+        #
+        #
+        #if not add it to the list
+        #
+        #
+        submission.comments.replace_more(limit=0)
+        for comment in submission.comments.list():
+            #search for the correct string
+            if comment.body.find("the") != -1:
+                    title = submission.title
+                    if len(title) > 55:
+                        title = title[:len(title)-(len(title)-52)] +"..."
+                    score = submission.score
+                    if score > 10000:
+                        score = str(score%1000) + "k"
+                    url = submission.url
+                    cmt = comment.body
+                    if len(cmt) > 13:
+                        cmt = title[:len(cmt)-(len(cmt)-50)] +"..."
+                    elm.append(str("<div class=\"rlink\"><img src=\"rlogo.jpg\" alt=\"link or default reddit icon\" /><div class=\"rinfo\"><span class=\"rtitle\">"+title+"</span><br /><span class=\"rtext\">"+cmt+"</span></div><div class=\"points\">"+str(score)+"</div></div>"))
+                    break
+print("---------------------------------\n")
 # Write our updated list back to the file
 with open("test.txt", "w") as f:
     for data in elm:
         f.write(data + "\n")
+#Write visited back to text file
