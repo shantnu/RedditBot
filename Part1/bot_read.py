7e52075cb193d1d4767163bc568e7e16a43c312a
aaaelite21 <alexander_ali@my.uri.edu>
2017-03-05 16:07:46 -0500

    Bot customizations and search for key words and output to php file

--- Part1/bot_read.py
+++ Part1/bot_read.py
@@ -1,12 +1,31 @@
 #!/usr/bin/python
 import praw
+import pdb
+import re
+import os
+from praw.models import MoreComments
 
+#create reddit instance
 reddit = praw.Reddit('bot1')
 
 subreddit = reddit.subreddit("modernmagic")
+elm=[]
 
 for submission in subreddit.hot(limit=5):
-    print("Title: ", submission.title)
-    linkData = submission.url
-    print("Url: ", linkData)    
+    #get comments and print relavent data
+    submission.comments.replace_more(limit=0)
+    for comment in submission.comments.list():
+        if comment.body.find("the") != -1:
+                title = submission.title
+                score = submission.score
+                url = submission.url
+                cmt = comment.body
+                if len(cmt) > 13:
+                    cmt = cmt[:len(cmt)-10] +"..."
+                elm.append(str("<div class=\"rlink\"><img src=\"rlogo.jpg\" alt=\"link or default reddit icon\" /><div class=\"rinfo\"><span class=\"rtitle\">"+title+"</span><br /><span class=\"rtext\">"+cmt+"</span></div><div class=\"points\">"+str(score)+"</div></div>"))
+                break
     print("---------------------------------\n")
+# Write our updated list back to the file
+with open("test.txt", "w") as f:
+    for data in elm:
+        f.write(data + "\n")
