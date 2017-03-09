35409b836bcade16df25e588c07530dfa1e68d88
aaaelite21 <alexander_ali@my.uri.edu>
2017-03-08 20:55:24 -0500

    php update css update js update... need to style better and use praw to track connections to prevent the disconect

--- skilled_bot/skilled_bot.py
+++ skilled_bot/skilled_bot.py
@@ -5,12 +5,15 @@ import re
 import os
 from praw.models import MoreComments
 
+#track number of aded posts
+added = 0
 #create reddit instance
 reddit = praw.Reddit('bot1')
 #get the desired sub reddits
-subs=["gaming"]
-#read in visite submissions as to avoid them
-# Have we run this code before? If not, create an empty list
+subs=["gaming", "modernmagic", "sports"]
+#keyword to be used when searching
+keyWord = "is"
+#Have we run this code before? If not, create an empty list
 if not os.path.isfile("posts_logged.txt"):
     posts_logged = []
 # If we have run the code before, load the list of posts we have replied to
@@ -21,49 +24,48 @@ else:
         posts_logged = posts_logged.split("\n")
         posts_logged = list(filter(None, posts_logged))
 #read in old html as to save it
-if not os.path.isfile("links.txt"):
+if not os.path.isfile("links.php"):
     elm = []
 # If we have run the code before, load the list of posts we have replied to
 else:
-    with open("links.txt", "r") as f:
+    with open("links.php", "r") as f:
         elm = f.read()
         elm = elm.split("\n")
         elm = list(filter(None, elm))
+        elm = list(reversed(elm))
 
 for s in subs:
     subreddit = reddit.subreddit(s)
-    for submission in subreddit.rising(limit=5):
+    for submission in subreddit.new(limit=5):
         #get comments and print relavent data
         #check if the submission has allready been checked
         if submission.id not in posts_logged:
             submission.comments.replace_more(limit=0)
-            #add search in title and intext
+            #add search in title and text
             #
             #
             for comment in submission.comments.list():
                 #search for the correct string
-                if comment.body.find("the") != -1: #key word needs changed
+                if comment.body.find(keyWord) != -1: #key word needs changed
                         title = submission.title
                         if len(title) > 55:
                             title = title[:len(title)-(len(title)-52)] +"..."
-                        score = submission.score
-                        if score > 10000:
-                            score = str(score%1000) + "k"
                         url = submission.url
                         cmt = comment.body
                         if len(cmt) > 13:
-                            cmt = title[:len(cmt)-(len(cmt)-50)] +"..."
-                        elm.append(str("<div class=\"rlink\"><img src=\"rlogo.jpg\" alt=\"link or default reddit icon\" /><div class=\"rinfo\"><span class=\"rtitle\">"+title+"</span><br /><span class=\"rtext\">"+cmt+"</span></div><div class=\"points\">"+str(score)+"</div></div>"))
+                            cmt = cmt[:len(cmt)-(len(cmt)-50)] +"..."
+                        elm.append(str("<div class=\"rlink "+ s +"\"><div class=\"rinfo\"><span class=\"rtitle\"><a href=\""+url+"\">"+title+"</a></span><br /><span class=\"rtext\">"+cmt+"</span></div><div class=\"tag\"></div></div>"))
                         # Store the current id into our list
                         posts_logged.append(submission.id)
+                        added = added + 1
                         break
-print("---------------------------------\n")
-# Write our updated list back to the file
-with open("links.txt", "w") as f:
+
+print("added: "+str(added)+"\n---------------------------------\n")
+elm=list(reversed(elm))
+with open("links.php", "w") as f:
     for data in elm:
         f.write(data + "\n")
-#Write visited back to text file
-# Write our updated list back to the file
+# update the file to make sure we dont double add links
 with open("posts_logged.txt", "w") as f:
     for post_id in posts_logged:
         f.write(post_id + "\n")
